dbc426dde1641db079d523a79a04860a9c021ef9
Subcogitate <jgstringer@gmail.com>
2018-08-02 19:33:54 -0500

    Updated content page. Added image. Connected Top Nav to state. Changed reducer to fire off of location change.

--- src/Main/TopNav.js
+++ src/Main/TopNav.js
@@ -1,20 +1,25 @@
 import * as React from 'react';
+import {connect} from 'react-redux';
 import { Link } from 'react-router-dom'
 import styles from '../styles/main.less';
 
 const myNav =[
-    {text:'About Me', url:'about'},
-    {text:'Education', url:'edu'},
-    {text:'Tech', url:'tech'},
-    {text:'Links', url:'links'},
-    {text:'Contact', url:'contact'}
+    {text:'About Me', url:'/about'},
+    {text:'Education', url:'/edu'},
+    {text:'Tech', url:'/tech'},
+    {text:'Links', url:'/links'},
+    {text:'Contact', url:'/contact'}
 ]
-const TopNav = () => (
+const TopNavComponent = (props) => (
     <nav className={styles.topNavContainer}>
         <ul className={styles.navList}>
-            {myNav.map((item, index) => <li key={index}><Link to={item.url} >{item.text}</Link></li>)}
+            {myNav.map((item, index) => <li key={index} className={item.url===props.selectedPage ? styles.active : ''}><Link to={item.url} >{item.text}</Link></li>)}
         </ul>
     </nav>
 )
 
-export default TopNav
+const mapStateToProps = (state) => {
+    return { selectedPage: state.pageReducer.selectedPage };
+  };
+
+export default connect(mapStateToProps)(TopNavComponent)
